4678b7e032a7bbddc4c143cccb0e4565643730f6
Yuzhu Hou <yuzhu.hou@gmail.com>
2017-11-27 12:13:12 +0100

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def answer
+    @query = params[:query]
+    if @query == "I am going to work right now!"
+      @answer = "Thank God!"
+    elsif @query.include? "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def ask
+
+  end
+end
